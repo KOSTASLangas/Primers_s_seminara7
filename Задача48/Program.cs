af11fcaf89aa4c12697b7e750f3ea82ea0cd3de1
Kostas <langas.kostya@mail.ru>
2022-08-13 18:36:36 +0500

    заполнение двумерного массива по формуле Сод.ячейки = m+n

---
+++ Задача48/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++) // цикл с 4 по 9 строки отвечает за заполнение массива
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        //array[i,j] = Convert.ToInt32(Console.ReadLine()); // ввод чисел для заполнения массива с клавиатуры
+        array [i, j] = i + j;  // ввод чисел для заполнения рандомом
+}
+
+for (int i = 0; i < array.GetLength(0); i++) // цикл с 11 по 16 строки отвечает за вывод всего массива в виде таблицы
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
